4a5e468f1deb2968b2027e58236dde0d17914be0
Michał Bartkiewicz <michal4421@outlook.com>
2017-01-14 15:17:18 +0100

    Add init migrations to project

---
+++ ForLan/ForLan/DbModel/ForlanDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLan.DbModel
+{
+    public class ForlanDbContext: DbContext
+    {
+        public ForlanDbContext()
+        {
+
+        }
+
+
+    }
+}
---
+++ ForLan/ForLan/Migrations/201701141414171_EmptyMigrations.cs
@@ -0,0 +1,16 @@
+namespace ForLan.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmptyMigrations : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
