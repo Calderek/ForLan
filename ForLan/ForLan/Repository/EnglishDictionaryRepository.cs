7214980a72c3f8de2cd1b01f8a9b318ad719bea7
Michał Bartkiewicz <michal4421@outlook.com>
2017-01-16 16:38:57 +0100

    Create repository to eng-pol add words

---
+++ ForLan/ForLan/Repository/EnglishDictionaryRepository.cs
@@ -0,0 +1,36 @@
+﻿using ForLan.DbModel;
+using ForLan.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLan.Repository
+{
+    public class EnglishDictionaryRepository
+    {
+        public void Add(string polishWord, string englishWord)
+        {
+            using (var db= new ForlanDbContext())
+            {
+                //todo check whether this word is avaible in database
+                EnglishWord wordEnglish = new EnglishWord() { Word = englishWord };
+                db.EnglishWord.Add(wordEnglish);
+
+                //todo check whether this word is avaible in database
+                PolishWord wordPolish = new PolishWord() { Word = englishWord };
+                db.PolishWord.Add(wordPolish);
+
+                DictionaryPlEng dictionary = new DictionaryPlEng();
+                dictionary.EnglishWord = wordEnglish;
+                dictionary.PolishWord = wordPolish;
+                db.DictionaryPolishEnglish.Add(dictionary);
+                db.SaveChanges();
+
+            }
+        }
+
+
+    }
+}
