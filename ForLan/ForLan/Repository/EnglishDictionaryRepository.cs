330ac507ef8772c9b9f25d7ed11b320df1e9a676
Calderek <michal4421@outlook.com>
2017-04-11 19:04:10 +0200

    Add method which added diffrent language type word. Can added english to polish and polish to england. Flexible for other language

--- ForLan/ForLan/Repository/EnglishDictionaryRepository.cs
+++ ForLan/ForLan/Repository/EnglishDictionaryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ForLan.DbModel;
 using ForLan.Entity;
+using ForLan.Helper;
 using ForLan.Repository.Abstract;
 
 namespace ForLan.Repository
@@ -28,6 +29,64 @@ namespace ForLan.Repository
 
             }
         }
+
+
+
+        public void Add(string word, string wordSaved, DictionaryHelper.DictionaryEnum dictionaryType)
+        {
+            using (var db = new ForlanDbContext())
+            {
+
+                switch (dictionaryType)
+                {
+                    case DictionaryHelper.DictionaryEnum.PolishToEnglish:
+                        {
+
+                            //todo check whether this word is avaible in database
+                            EnglishWord wordEnglish = new EnglishWord() { Word = word };
+                            db.EnglishWord.Add(wordEnglish);
+
+                            //todo check whether this word is avaible in database
+                            PolishWord wordPolish = new PolishWord() { Word = wordSaved };
+                            db.PolishWord.Add(wordPolish);
+
+                            DictionaryPlEng dictionary = new DictionaryPlEng();
+                            dictionary.EnglishWord = wordEnglish;
+                            dictionary.PolishWord = wordPolish;
+                            db.DictionaryPolishEnglish.Add(dictionary);
+                            break;
+                        }
+                    case DictionaryHelper.DictionaryEnum.EnglishToPolish:
+                        {
+                            //todo check whether this word is avaible in database
+                            EnglishWord wordEnglish = new EnglishWord() { Word = wordSaved };
+                            db.EnglishWord.Add(wordEnglish);
+
+                            //todo check whether this word is avaible in database
+                            PolishWord wordPolish = new PolishWord() { Word = word };
+                            db.PolishWord.Add(wordPolish);
+
+                            DictionaryPlEng dictionary = new DictionaryPlEng();
+                            dictionary.EnglishWord = wordEnglish;
+                            dictionary.PolishWord = wordPolish;
+                            db.DictionaryPolishEnglish.Add(dictionary);
+                        }
+                        break;
+                    default:
+                        {
+
+                        }
+                        break;
+                }
+
+
+                db.SaveChanges();
+
+                
+
+            }
+        }
+
 
 
     }
